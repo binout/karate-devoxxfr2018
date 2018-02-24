a293342c0137a51a20f891cb65314ec614a0a17b
Benoit Prioux <benoit.prioux@gmail.com>
2018-02-24 16:58:36 +0100

    init build process

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Generate slides"
+docker rm asciidoc-to-deckjs
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+docker run -v $DIR:/documents/ --name asciidoc-to-deckjs binout/docker-asciidoctor-deckjs asciidoctor -T /asciidoctor-backends/haml/deckjs -D /documents/output *.adoc
+
+echo "Copy resources"
+cd output ; sed 's/"\/\/cdnjs/"https:\/\/cdnjs/'  README.html > index.html ; rm README.html; cp -R ../images images
+
+if [ ! -d "deck.js" ]; then
+  echo "Retrieve deck.js"
+  mkdir deck.js ; curl -LkSs https://github.com/imakewebthings/deck.js/archive/1.1.0.tar.gz | tar xfz - -C deck.js --strip-components=1
+fi
