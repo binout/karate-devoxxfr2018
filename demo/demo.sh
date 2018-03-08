20a96ee6cd0df42da42ce0d937f19d454dc77b48
Benoit Prioux <benoit.prioux@gmail.com>
2018-03-08 08:22:19 +0100

    karate demo with standaone jar

--- demo/demo.sh
+++ demo/demo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl https://search.maven.org/remotecontent?filepath=com/intuit/karate/karate-core/0.7.0.1/karate-core-0.7.0.1.jar --output karate.jar
-java -cp karate.jar com.intuit.karate.ui.App
+#curl https://github.com/intuit/karate/releases/download/v0.7.0/karate-netty-0.7.0-all.jar --output karate-all.jar
+java -jar karate-netty-0.7.0-all.jar -t cfp-speaker.feature
