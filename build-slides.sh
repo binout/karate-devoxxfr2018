12295d26e52eba9c31c24e8fa498d835dc04b9ce
Benoit Prioux <benoit.prioux@gmail.com>
2018-03-21 22:03:12 +0100

    add images

--- build-slides.sh
+++ build-slides.sh
@@ -5,9 +5,9 @@ echo "Generate slides"
 docker run --rm -v $(pwd):/documents/ --name asciidoc-to-deckjs binout/docker-asciidoctor-deckjs asciidoctor -T /asciidoctor-backends/haml/deckjs -D /documents/output slides.adoc
 
 echo "Copy resources"
-cd output ; sed 's/"\/\/cdnjs/"https:\/\/cdnjs/'  slides.html > index.html ; cp -R ../images images
+sed 's/"\/\/cdnjs/"https:\/\/cdnjs/'  output/slides.html > output/index.html ; cp -R images output
 
 if [ ! -d "deck.js" ]; then
   echo "Retrieve deck.js"
-  mkdir deck.js ; curl -LkSs https://github.com/imakewebthings/deck.js/archive/1.1.0.tar.gz | tar xfz - -C deck.js --strip-components=1
+  mkdir -p output/deck.js ; curl -LkSs https://github.com/imakewebthings/deck.js/archive/1.1.0.tar.gz | tar xfz - -C output/deck.js --strip-components=1
 fi
